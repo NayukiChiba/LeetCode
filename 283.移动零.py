39a5d5e20269745ab1bf2aa1cf5fe923e8e03500
NayukiMeko <MekoNayuki@outlook.com>
2026-08-02 18:29:36 +0800

    docs(array): 记录283.移动零的原地操作解法 - 使用双指针法，将非零元素前移并填充零 - 时间复杂度O(n)，空间复杂度O(1) - 提供Python实现及详细注释

---
+++ 283.移动零.py
@@ -0,0 +1,84 @@
+#
+# @lc app=leetcode.cn id=283 lang=python3
+# @lcpr version=30404
+#
+# [283] 移动零
+#
+# https://leetcode.cn/problems/move-zeroes/description/
+#
+# algorithms
+# Easy (63.76%)
+# Likes:    2959
+# Dislikes: 0
+# Total Accepted:    2.4M
+# Total Submissions: 3.8M
+# Testcase Example:  '[0,1,0,3,12]\n[0]'
+#
+# 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+# 
+# 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+# 
+# 
+# 
+# 示例 1:
+# 
+# 输入: nums = [0,1,0,3,12]
+# 输出: [1,3,12,0,0]
+# 
+# 
+# 示例 2:
+# 
+# 输入: nums = [0]
+# 输出: [0]
+# 
+# 
+# 
+# 提示:
+# 
+# 
+# 
+# 1 <= nums.length <= 10^4
+# -2^31 <= nums[i] <= 2^31 - 1
+# 
+# 
+# 
+# 
+# 进阶：你能尽量减少完成的操作次数吗？
+# 
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        fast = 0
+        slow = 0
+        zero_count = 0
+        while fast < len(nums):
+            # 遇到非 0 就把slow代替掉
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        for i in range(slow, len(nums)):
+            nums[i] = 0
+                
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [0,1,0,3,12]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0]\n
+# @lcpr case=end
+
+#
+
