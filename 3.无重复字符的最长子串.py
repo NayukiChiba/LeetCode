00e09255dd49b2aade9976272c5557fe2fd8fd9d
NayukiMeko <MekoNayuki@outlook.com>
2026-09-07 20:14:05 +0800

    docs(leetcode): 新增「3. 无重复字符的最长子串」题解与滑动窗口解法说明 - 通过滑动窗口和哈希集合高效找出最长无重复子串 - 单次遍历字符串，时间复杂度为O(n) - 空间复杂度为O(min(n, m))，m为字符集大小 - 附有完整Python代码及关键步骤注释

---
+++ 3.无重复字符的最长子串.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+# @lcpr version=30404
+#
+# [3] 无重复字符的最长子串
+#
+# https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+#
+# algorithms
+# Medium (42.65%)
+# Likes:    11614
+# Dislikes: 0
+# Total Accepted:    4.4M
+# Total Submissions: 10.3M
+# Testcase Example:  '"abcabcbb"\n"bbbbb"\n"pwwkew"'
+#
+# 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+#
+#
+#
+# 示例 1:
+#
+# 输入: s = "abcabcbb"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。注意 "bca" 和 "cab" 也是正确答案。
+#
+#
+# 示例 2:
+#
+# 输入: s = "bbbbb"
+# 输出: 1
+# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+#
+#
+# 示例 3:
+#
+# 输入: s = "pwwkew"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+#
+#
+#
+#
+# 提示：
+#
+#
+# 0 <= s.length <= 10^5
+# s 由英文字母、数字、符号和空格组成
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sub_str = ""
+        res = 0
+        for char in s:
+            while char in sub_str:
+                # 把第一个删掉
+                if len(sub_str) <= 1:
+                    sub_str = ""
+                else:
+                    sub_str = sub_str[1:]
+
+            sub_str = sub_str + char
+            res = max(len(sub_str), res)
+        return res
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# "abcabcbb"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "bbbbb"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "pwwkew"\n
+# @lcpr case=end
+
+#
